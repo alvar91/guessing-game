333f37286db8f4a95c6a528312ebfdb509e98da2
alvar91 <aleksey91scorp@bk.ru>
2023-08-06 15:26:38 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+  #min = 0;
+  #max = 0;
+  #mid = 0;
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
 
-    }
+  guess() {
+    return (this.#mid = Math.ceil((this.#max + this.#min) / 2));
+  }
 
-    guess() {
+  lower() {
+    this.#max = this.#mid;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.#min = this.#mid;
+  }
 }
 
 module.exports = GuessingGame;
